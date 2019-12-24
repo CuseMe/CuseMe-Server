9f0b61f1591646e55ecbe6e375e797436002fb8c
Haely <khl6235@naver.com>
2019-12-24 19:54:12 +0900

    tmp commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,6 @@ const { util, status, message} = require('../modules/utils');
 const NAME = '사용자'; 
 
 module.exports = {
-    signUp: async(req, res) => 
-        User.signUp(req.body)
-        .then(() => 
-            res.status(status.OK)
-            .send(util.successTrue(message.SIGNUP_SUCCESS)))
-        .catch(err => 
-            res.status(err.status || 500)
-            .send(util.successFalse(err.message)))
-    ,
     signIn: async(req, res) => 
         User.signIn(req.body)
         .then((token) =>
@@ -20,5 +11,5 @@ module.exports = {
         .catch(err => 
             res.status(err.status || 500)
             .send(util.successFalse(err.message)))
-            
+
 }
