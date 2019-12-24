40030dd2f8a62d3bbddfdab0a01de3d54106385d
siyeons <siyeonleeme@gmail.com>
2019-12-25 05:02:47 +0900

    전체 raw card data 모듈

---
+++ modules/data/cardData.js
@@ -0,0 +1,15 @@
+module.exports =
+    (rawCardData) => {
+        cardData = {
+            "cardIdx": rawCardData.cardIdx,
+            "title": rawCardData.title,
+            "content": rawCardData.content,
+            "image": rawCardData.image,
+            "record": rawCardData.record,
+            "count": rawCardData.count,
+            "visible": rawCardData.visible,
+            "serialNum": rawCardData.serialNum,
+            "userIdx": rawCardData.userIdx,
+        }
+        return cardData
+}
