7d0f3609f46bc98442f5fc1b3368b666d26b7534
Haely <khl6235@naver.com>
2019-12-24 19:05:04 +0900

    outline

---
+++ modules/security/jwt.js
@@ -0,0 +1,52 @@
+const randToken = require('rand-token');
+const jwt = require('jsonwebtoken');
+const {secretOrPrivateKey} = require('../config/secretKey');
+const options = {
+    //algorithm: "HS256",
+    //expiresIn: "1m",
+    //issuer: "haely"
+};
+
+module.exports = {
+    //토큰 생성
+    sign: (user) => {
+        const payload = {
+            idx: user.idx,
+            grade: user.grade,
+            name: user.name
+        };
+        const result = {
+            //json값인 payload. jwt.sign을 쓰면 토큰이 하나 만들어짐!
+            token: jwt.sign(payload, secretOrPrivateKey, options),
+            refreshToken: randToken.uid(256)
+        };
+        return result;
+    },
+    //토큰 검증
+    verify: (token) => {
+        let decoded;
+        try{
+            decoded = jwt.verify(token, secretOrPrivateKey);
+        } catch(err){
+            if(err.message === 'jwt expired'){
+                console.log('expired token');
+                return -3;
+            } else if(err.message === 'invalid token'){
+                console.log('invalid token');
+                return -2;
+            } else{
+                console.log('other problem');
+                return -2;
+            }
+        }
+        return decoded;
+    },
+    refresh: (user) => {
+        const payload = {
+            idx: user.idx,
+            grade: user.grade,
+            name: user.name
+        };
+        return jwt.sign(payload, secretOrPrivateKey, options);
+    }
+};
---
+++ modules/utils/authUtil.js
@@ -0,0 +1,44 @@
+// authUtil.js
+const jwt = require('../jwt');
+const responseMessage = require('./responseMessage');
+const statusCode = require('./statusCode');
+const util = require('./utils');
+
+const authUtil = {
+
+    LoggedIn: async(req, res, next) =>{
+        const {token} = req.headers;
+
+        //1. 토큰 존재하는지 확인
+        if(!token){
+            return res.status(statusCode.BAD_REQUEST).send(util.successFalse(responseMessage.EMPTY_TOKEN));
+        }
+
+        //2. 토큰 유효한지 확인
+        const result = jwt.verify(token);
+
+        if(result == -3){
+            res.status(statusCode.UNAUTHORIZED)
+            .send(util.successFalse(responseMessage.EXPIRED_TOKEN));
+            return;
+        }
+        if(result == -2){
+            res.status(statusCode.UNAUTHORIZED)
+            .send(util.successFalse(responseMessage.INVALID_TOKEN));
+            return;
+        }
+
+        console.log(result);
+        const userIdx = result.idx;
+        if(!userIdx){
+            res.status(statusCode.BAD_REQUEST)
+            .send(util.successFalse(responseMessage.NULL_VALUE));
+            return;
+        }
+
+        req.decoded = userIdx;
+        next();
+    }
+
+}
+module.exports = authUtil;
