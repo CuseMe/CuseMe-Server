0d5217a399d127ec5a76a5a1bf790247edc3ad5b
siyeons <siyeonleeme@gmail.com>
2019-12-25 22:05:46 +0900

    Authorization error 메세지 수정

--- errors/AuthorizationError.js
+++ errors/AuthorizationError.js
@@ -2,15 +2,14 @@ const statusCode = require('../modules/utils/statusCode');
 const responseMessage = require('../modules/utils/responseMessage');
 
 class AuthorizationError extends Error {
-    constructor(name, code = 'GENERIC', status = statusCode.UNAUTHORIZED, ...params) {
+    constructor(code = 'GENERIC', status = statusCode.UNAUTHORIZED, ...params) {
         super(...params);
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, AuthorizationError);
         }
-        this.name = name;
         this.code = code;
         this.status = status;
-        this.message = responseMessage.UNAUTHORIZED(name);
+        this.message = responseMessage.NO_USER;
     }
 }
 
