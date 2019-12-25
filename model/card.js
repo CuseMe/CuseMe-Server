61d52383ef347497ce4cc4102817740cdf2a6cad
siyeons <siyeonleeme@gmail.com>
2019-12-25 22:05:25 +0900

    카드 삭제 기능 구현

--- model/card.js
+++ model/card.js
@@ -24,16 +24,12 @@ const card = {
         const query = `SELECT * FROM ${TABLE}`;
         const result = await pool.queryParam_None(query);
         if(result.length == 0) throw new NotFoundError(CARD);
-        const cardArr = [];
-        result.forEach((rawCard, index, result) => 
-            cardArr.push(cardData(rawCard)));
-        return cardArr;
+        return result.map(cardData);
     },
     count: async (cardIdx) => {
         const query = `UPDATE card SET count = count + 1 WHERE cardIdx = ${cardIdx}`;
         const values = [cardIdx];
         const result = await pool.queryParam_Parse(query, values);
-        // if(result.length == 0) throw new NotFoundError(CARD);
         if(result.affectedRows == 0) throw new NotFoundError();
     },
     create: async(
@@ -43,18 +39,24 @@ const card = {
         visible
     ) => {
         //TODO: 카드 생성
-    }
-    ,
+    },
     update: {
         //TODO: 카드 상세 수정
-    }
-    ,
+    },
     updateAll: {
         //TODO: 카드 배열 및 전체 수정
-    }
-    ,
-    delete: {
-        //TODO: 카드 상세 삭제
+    },
+    delete: async ({cardIdx}, token) => {
+        const user = jwtExt.verify(token).data.userIdx
+        const verifyQuery = `SELECT * FROM ${TABLE} WHERE cardIdx = ? AND userIdx = ?`;
+        const verifyValues = [cardIdx, user];
+        const verifyResult = await pool.queryParam_Parse(verifyQuery, verifyValues);
+        if(verifyResult.length == 0) throw new AuthorizationError();
+
+        const deleteQuery = `DELETE FROM ${TABLE} WHERE cardIdx = '${cardIdx}'`;
+        const deleteResult = await pool.queryParam_None(deleteQuery);
+        if(deleteResult.affectedRows == 0) throw new NotDeletedError();
+        return deleteResult;
     }
 }
 
