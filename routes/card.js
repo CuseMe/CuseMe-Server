b1e8c605168ee57997cf3d9ce0344282840478d6
ganghee <idiosauri@naver.com>
2019-12-26 21:55:31 +0900

    이미지 mp3 handling

--- routes/card.js
+++ routes/card.js
@@ -3,12 +3,15 @@ const router = express.Router({mergeParams: true});
 const CardController = require('../controllers/cardController');
 const upload = require('../config/multer')
 
+var cpUpload = upload.fields([{ name: 'image', maxCount: 1 }, { name: 'record', maxCount: 1 }])
 router.get('/',CardController.readAll)
 router.get('/:cardIdx',CardController.read);
 router.put('/:cardIdx/count',CardController.count);
-router.post('/',upload.single('image'), upload.single('record'), CardController.create);
-router.put('/:cardIdx',CardController.update);
+router.post('/',cpUpload, CardController.create);
+router.put('/:cardIdx',cpUpload, CardController.update);
 router.put('/',CardController.update);
 router.delete('/',CardController.delete);
+
+console.log('baseUri/cards');
 
 module.exports = router
