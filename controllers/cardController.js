f99e7807d8db02f837ce64e2118cf5c6e997c95f
chaeyeonhwang <victory6309@gmail.com>
2019-12-26 15:40:13 +0900

    change

---
+++ controllers/cardController.js
@@ -0,0 +1,52 @@
+const Card = require('../model/card');
+const {util, status, message} = require('../modules/utils');
+const NAME = '카드';
+
+module.exports = {
+    readAll: async(req, res) => {
+        const {blogIdx} = req.params;
+        Article.readAll(blogIdx)
+        .then(result =>
+            res.status(status.OK)
+            .send(util.successTrue(message.X_READ_SUCCESS(NAME), result)))
+        .catch(err =>
+            res.status(err.status || 500)
+            .send(util.successFalse(err.message)))}
+    ,
+    read: async(req, res) => {
+        const {articleIdx} = req.params;
+        Article.read(articleIdx)
+        .then(result => 
+            res.status(status.OK)
+            .send(util.successTrue(message.X_READ_SUCCESS(NAME), result)))
+        .catch(err => 
+            res.status(err.status || 500)
+            .send(util.successFalse(err.message)))}
+    ,
+    create: async(req, res) => 
+        Article.create(req.files, req.body, req.params.blogIdx, req.headers.token)
+        .then(() =>
+            res.status(status.OK)
+            .send(util.successTrue(message.X_CREATE_SUCCESS(NAME))))
+        .catch(err => 
+            res.status(err.status || 500)
+            .send(util.successFalse(err.message)))
+    ,
+    update: async(req, res) => 
+        Article.update(req.files, req.body, req.params.blogIdx, req.headers.token)
+        .then(() =>
+            res.status(status.OK)
+            .send(util.successTrue(message.X_UPDATE_SUCCESS(NAME))))
+        .catch(err => 
+            res.status(err.status || 500)
+            .send(util.successFalse(err.message)))
+    ,
+    delete: async(req, res) => 
+        Article.delete(req.body, req.params.blogIdx, req.headers.token)
+        .then(() =>
+            res.status(status.OK)
+            .send(util.successTrue(message.X_DELETE_SUCCESS(NAME))))
+        .catch(err => 
+            res.status(err.status || 500)
+            .send(util.successFalse(err.message)))
+}
