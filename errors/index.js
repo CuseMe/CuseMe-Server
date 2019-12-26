3a9bc318cc1532f734412224969b11233b827cf3
ganghee <idiosauri@naver.com>
2019-12-26 21:52:15 +0900

    쓰지 않는 에러 모듈 제거

--- errors/index.js
+++ errors/index.js
@@ -1,16 +1,12 @@
 module.exports = {
     ParameterError: require('./ParameterError'),
     DatabaseError: require('./DatabaseError'),
-    NoReferencedRowError: require('./NoReferencedRowError'),
-    NotMatchedError: require('./NotMatchedError'),
-    AuthorizationError: require('./AuthorizationError'),
+    AuthorizationError: require('./NoUserError'),
     TokenExpiredError: require('./TokenExpiredError'),
-    DuplicatedEntryError: require('./DuplicatedEntryError'),
     MissPasswordError: require('./MissPasswordError'),
     NotFoundError: require('./NotFoundError'),
     NotCreatedError: require('./NotCreatedError'),
     NotUpdatedError: require('./NotUpdatedError'),
     NotDeletedError: require('./NotDeletedError'),
-    ExistedUserError: require('./ExistedUserError'),
 };
 
