d043647ba0e97cdc189fc23116fe1453675c4e8a
ganghee <idiosauri@naver.com>
2019-12-27 14:49:58 +0900

    added middelware

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const AuthControllers = require('../controllers/userController');
-const authUtil = require('../modules/utils/authUtil');
+const {LoggedIn} = require('../modules/utils/authUtil');
 
 router.get('/start',AuthControllers.start);
 router.get('/signIn', AuthControllers.signIn);
-router.put('/',AuthControllers.updatePwd)
-router.put('/phone', AuthControllers.updatePhone);
+router.put('/', LoggedIn, AuthControllers.updatePwd)
+router.put('/phone', LoggedIn, AuthControllers.updatePhone);
 
 module.exports = router;
