5620faf1735975aa9146eba7b20c68c02f95a51b
siyeons <siyeonleeme@gmail.com>
2019-12-28 18:03:58 +0900

    첫 시작 시 유저 확인 후 유저 및 기본 카드 생성

--- model/user.js
+++ model/user.js
@@ -4,32 +4,52 @@ const authUtil = require('../modules/utils/authUtil');
 const encryptionManager = require('../modules/security/encryptionManager');
 const db = require('../modules/security/db/pool');
 const error = require('../errors');
+const crypto = require('crypto');
 
 const TABLE = 'user';
 const NAME = "사용자";
 
 module.exports = {
     start: async(uuid) => {
-        if(!uuid) throw new error.ParameterError;
-        const getQuery = `SELECT * FROM card WHERE uuid = ?`;
-        const getValues = [uuid];
-        const getResult = await db.queryParam_Parse(getQuery, getValues);
-        //console.log('getResult@@@',getResult)
-        //if(getResult.length == 0) throw new NotFoundError(NAME);
-        if(getResult.length == 0){ 
-            const salt = await encryptionManager.makeRandomByte();
-            const hashedPassword = encryptionManager.encryption('0000', salt); 
-            const postQuery = `INSERT INTO ${TABLE}(uuid, password, salt, phoneNum) VALUES (?, ?, ?, 0)`; 
-            const postValues = [uuid, hashedPassword, salt];
-            const postResult = await db.queryParam_Parse(postQuery, postValues);
-            const postCardValues = [{title:"text"},{content:"text"},{image:"text"},{record:"text"},{count: 1},{visible: 1},{serialNum:"text"},{sequence: 0},{uuid: 00000}];
-            for (var i=0; i<4; i++) {
-                const postCardQuery = `INSERT INTO CARD(title, content, image, record, count, visible, serialNum, sequence,  uuid) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ${getValues})`;
-                const postCardValue= [postCardValues[i].title, postCardValues[i].content, postCardValues[i].image, postCardValues[i].record, postCardValues[i].count, postCardValues[i].visible, postCardValues[i].serialNum, postCardValues[i].sequence]; 
+        const findUserQuery = `SELECT * FROM user WHERE uuid = ?`;
+        const findUserValues = [uuid];
+        const findUserResult = await db.queryParam_Parse(findUserQuery, findUserValues);
+        const phoneNum = '000000000000'
+
+        if (findUserResult.length == 0 || !findUserResult) {
+            const salt = (await crypto.randomBytes(32)).toString('hex');
+            const hashedPassword = encryptionManager.encryption('0000', salt);
+            const userInsertQuery = `INSERT INTO ${TABLE}(uuid, password, salt, phoneNum) VALUES (?, ?, ?, ?)`;
+            const userInsertValues = [uuid, hashedPassword, salt, phoneNum];
+            const userInsertResult = await db.queryParam_Parse(userInsertQuery, userInsertValues);
+            let userIdx = userInsertResult.insertId;
+
+            for (var i = 12; i < 15; i++) {
+                const postQuery = `INSERT INTO own (cardIdx, userIdx) VALUES(?, ?)`;
+                let postValues = [i, userIdx]
+                const postResult = await db.queryParam_Parse(postQuery, postValues);
+                console.log(postResult)
+                if(postResult.affectedRows == 0) throw new error.NotUpdatedError;
             }
-            const cardResult = await db.queryParam_Parse(postCardQuery,postCardValues);
-            if(postResult.affectedRows == 0) throw new error.NotUpdatedError;
         }
+
+        // const getQuery = `SELECT * FROM own WHERE uuid = ?`;
+        // const getValues = [uuid];
+        // const getResult = await db.queryParam_Parse(getQuery, getValues);
+        // if(getResult.length == 0){ 
+        //     const salt = await encryptionManager.makeRandomByte();
+        //     const hashedPassword = encryptionManager.encryption('0000', salt); 
+        //     const postQuery = `INSERT INTO ${TABLE}(uuid, password, salt, phoneNum) VALUES (?, ?, ?, 0)`; 
+        //     const postValues = [uuid, hashedPassword, salt];
+        //     const postResult = await db.queryParam_Parse(postQuery, postValues);
+        //     const postCardValues = [{title:"text"},{content:"text"},{image:"text"},{record:"text"},{count: 1},{visible: 1},{serialNum:"text"},{sequence: 0},{uuid: 00000}];
+        //     for (var i=0; i<4; i++) {
+        //         const postCardQuery = `INSERT INTO CARD(title, content, image, record, count, visible, serialNum, sequence,  uuid) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ${getValues})`;
+        //         const postCardValue= [postCardValues[i].title, postCardValues[i].content, postCardValues[i].image, postCardValues[i].record, postCardValues[i].count, postCardValues[i].visible, postCardValues[i].serialNum, postCardValues[i].sequence]; 
+        //     }
+        //     const cardResult = await db.queryParam_Parse(postCardQuery,postCardValues);
+        //     if(postResult.affectedRows == 0) throw new error.NotUpdatedError;
+        // }
     },
     signIn: async (uuid) => {
         if(!uuid) throw new error.ParameterError
