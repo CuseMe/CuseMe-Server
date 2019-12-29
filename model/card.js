95bbc8b6b5d29c4461e5cbd9e126703d30497470
siyeons <siyeonleeme@gmail.com>
2019-12-30 00:25:14 +0900

    card 조회 쿼리 수정

--- model/card.js
+++ model/card.js
@@ -24,16 +24,18 @@ const card = {
             return card;
     },
     readAll: async (token) => {
-        const uuid = jwtExt.verify(token).data.uuid;
-        const query = `SELECT * FROM ${TABLE} WHERE uuid = ?`;
-        const values = [uuid]
+        const userIdx = jwtExt.verify(token).data.userIdx;
+        const query = `SELECT * FROM ${TABLE} JOIN own ON card.cardIdx = own.cardIdx WHERE own.userIdx = ?`;
+        const values = [userIdx]
         const result = await pool.queryParam_Parse(query,values);
         if(result.length == 0) throw new NotFoundError;
         return result.map(cardData);
     },
-    readVisible: async (uuid) => {
-        const query = `SELECT * FROM ${TABLE} WHERE uuid = '${uuid}' AND visible = 1`;
-        const result = await pool.queryParam_None(query);
+    readVisible: async (token) => {
+        const userIdx = jwtExt.verify(token).data.userIdx;
+        const query = `SELECT * FROM ${TABLE} JOIN own ON card.cardIdx = own.cardIdx WHERE own.userIdx = ?`;
+        const values = [userIdx]
+        const result = await pool.queryParam_Parse(query,values);
         if(result.length == 0) throw new NotFoundError;
         return result.map(cardData);
     },
