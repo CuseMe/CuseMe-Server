b0ffddfcc1be4c140885dd2ca83babb1c2d12c40
ganghee <idiosauri@naver.com>
2020-01-01 12:55:04 +0900

    updated visible

--- modules/data/cardData.js
+++ modules/data/cardData.js
@@ -7,7 +7,7 @@ module.exports =
             "image": rawCardData.image,
             "record": rawCardData.record,
             "count": rawCardData.count,
-            "visible": rawCardData.visible,
+            "visible": rawCardData.visible == 1 ? true : false,
             "serialNum": rawCardData.serialNum,
             "sequence": rawCardData.sequence,
             "userIdx": rawCardData.userIdx
