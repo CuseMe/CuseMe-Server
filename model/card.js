fbf3ab00dd3d67c2e0ffc90badd598f211c9c008
ganghee <idiosauri@naver.com>
2020-01-01 12:57:41 +0900

    deleted log

--- model/card.js
+++ model/card.js
@@ -22,7 +22,6 @@ const card = {
             const values = [cardIdx, userIdx]
             const result = await pool.queryParam_Parse(query, values);
             if(result.length == 0) throw new NotFoundError;
-            console.log("result11",result)
             const card = cardData(result[0]);
             return card;
     },
@@ -39,7 +38,6 @@ const card = {
         const query = `SELECT * FROM ${CARD_TABLE} JOIN ${OWN_TABLE} ON ${CARD_TABLE}.cardIdx = ${OWN_TABLE}.cardIdx where ${OWN_TABLE}.visible = 1 AND ${OWN_TABLE}.userIdx = ?`;        
         const values = [userIdx];
         const result = await pool.queryParam_Parse(query, values);
-        console.log("userIdx",userIdx);
         if(result.length == 0) [];
         return result.map(cardData);
     },
@@ -73,7 +71,6 @@ const card = {
             const postQuery = `INSERT INTO ${OWN_TABLE}(cardIdx, userIdx, sequence, visible) VALUES(?, ?, ?, ?)`;
             const visible_boolean = ~~Boolean(visible)
             const postValues = [cardIdx, userIdx, count, visible_boolean];
-            console.log('postValues',postValues);
             const postResult = await pool.queryParam_Parse(postQuery, postValues);
             if(postResult.affectedRows == 0) throw new NotCreatedError; 
     },
